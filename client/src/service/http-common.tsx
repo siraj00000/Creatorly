0c135dc6d73565e48b40bedf724df5336ebcf5ba
siraj00000 <siraj00000ahmed@gmail.com>
2023-06-07 00:28:19 +0500

    IP updated

--- client/src/service/http-common.tsx
+++ client/src/service/http-common.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API = axios.create({
-  baseURL: 'http://localhost:8008/',
+  baseURL: 'http://16.171.68.194/',
   headers: {
     'Content-type': 'application/json',
   },
